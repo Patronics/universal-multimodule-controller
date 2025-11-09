a51a0001aaffd3093f5ce9c65997bc6ddea041c1
Patrick Leiser <patrick27leiser@yahoo.com>
2025-11-09 14:20:59 -0800

    pull forward additional changes from debug branch, adding AllocConfigValueInput function and improving type safety

--- ChannelManager.h
+++ ChannelManager.h
@@ -1,7 +1,10 @@
 #include <stdint.h>
+#include "UI.h"
 
 #ifndef CHANNEL_MANAGER_H
 #define CHANNEL_MANAGER_H
+
+//#define MAX(a,b) ((a) > (b) ? (a) : (b))
 
 //MAX_INPUTS > MAX_CHANNELS to allow for unused inputs from other input sources. 128 allows for USB keyboard, multiple controllers, etc
 #define MAX_INPUTS 128
@@ -16,14 +19,19 @@ typedef enum {
     INPUT_FUNCTION_IO_SWITCH,     //boolean value from hardwired switch
     INPUT_FUNCTION_USB_VALUE,     //analog value from USB device input    //todo implement/precisely define
     INPUT_FUNCTION_USB_SWITCH,    //boolean value from USB device switch  //todo implement/precisely define
+    INPUT_FUNCTION_IR_SWITCH,     //boolean value from IR remote          //todo implement/precisely define
     INPUT_FUNCTION_UNKNOWN,
 } InputFunctionType;
 
 //the input function to get updated data. If id is used when generating a value, any given function MUST only be used to handle a single type as defined by InputFunctionType
 typedef int (*GetChannelInputFn)(void* context, int id);
 
+//function for configuring settings for a given input. If no settings available, null ptr instead
+typedef int (*ConfigChannelInputFn)(void* context, int id, NavButton btnPressed);
+
 typedef struct {
     GetChannelInputFn getLatestInputData; // Function pointer to retrieve latest input data, pass the ID as argument
+    ConfigChannelInputFn configureChannelInput;
     int id; // Unique ID number
     InputFunctionType inputFunctionType;  //type is used as a namespace for ID numbers, and to describe which operations are supported
     void * context; //reserved to allow passing additional context to the function, with exact layout expected to be consistient within a given InputFunctionType
